027d605b44a5a9c2944fe315940dba5d2cd377a7
45183 <hsb9743@naver.com>
2022-02-25 11:11:45 +0900

    day39

---
+++ model/model_board.py
@@ -0,0 +1,65 @@
+from model.model_member import getconn
+from flask import request, session
+
+
+def select_board():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "SELECT * FROM board ORDER BY bno DESC"
+    cur.execute(sql)
+    rs = cur.fetchall()
+    conn.close()
+    return rs
+
+def select_bo_one(bno):
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "SELECT * FROM board WHERE bno = '%s'" % (bno)
+    cur.execute(sql)
+    rs = cur.fetchone()
+    
+    # 조회수 설정
+    # hit 1증가 시켜서 UPDATE SET 사용
+    hit = rs[4]             # 이전 조회수
+    hit = hit + 1
+    sql = "UPDATE board SET hit = '%s' WHERE bno = '%s'" % (hit, bno)
+    cur.execute(sql)
+    conn.commit()
+    conn.close()
+    return rs
+
+def write_board():
+    # 데이터를 넘겨 받음
+    title = request.form['title']
+    content = request.form['content']
+    hit = 0
+    mid = session.get('userName')  # 로그인한 mid(작성자)
+
+    # db 연동 처리
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "INSERT INTO board(title, content, hit, mid) VALUES ('%s', '%s', '%s', '%s')" % (title, content, hit, mid)
+    cur.execute(sql)
+    conn.commit()
+    conn.close()
+
+def delete_board(bno):
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "DELETE FROM board WHERE bno = '%s'" % (bno)
+    cur.execute(sql)
+    conn.commit()
+    conn.close()
+
+def update_board(bno):
+    # 데이터 전달받기
+    title = request.form['title']
+    content = request.form['content']
+    mid = session.get('userName')
+
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "UPDATE board SET title = '%s', content = '%s', mid = '%s' WHERE bno = '%s'" % (title, content, mid, bno)
+    cur.execute(sql)
+    conn.commit()
+    conn.close()
